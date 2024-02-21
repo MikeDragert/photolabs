22ab0da8253b4d11c09309e9d24b4cacf2df43fe
Mike Dragert <mikedragert@gmail.com>
2024-02-21 18:09:27 -0500

    Implement PhotoList

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoList.scss";
+import PhotoListItem from "./PhotoListItem";
 
 const sampleDataForPhotoList = [
   {
@@ -57,9 +58,20 @@ const sampleDataForPhotoList = [
 ];
 
 const PhotoList = () => {
+  const photoList = function() {
+    return sampleDataForPhotoList.map((photo) => {
+      return <PhotoListItem photoItem = {{
+        id: photo.id,
+        username: photo.user.useername,
+        location: photo.location,
+        imageSource: photo.urls.regular
+      }} key={photo.id}/>
+    }); 
+  }
+  
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {photoList()}
     </ul>
   );
 };
