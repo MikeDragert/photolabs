d056484f75eccfcbd224c58785f5ffb10eade590
Mike Dragert <mikedragert@gmail.com>
2024-02-21 19:03:14 -0500

    Finish the top Nav Bar elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
+import TopNavigation from './components/TopNavigationBar';
+
 import './App.scss';
 
 
@@ -9,7 +10,7 @@ import './App.scss';
 const App = () => {  
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
