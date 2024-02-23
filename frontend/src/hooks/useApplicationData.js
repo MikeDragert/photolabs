7ad3566d4b1ac2afc7f2103241f6117f8d612fee
Mike Dragert <mikedragert@gmail.com>
2024-02-23 14:44:20 -0500

    Implement constants for dispatch actions.

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,6 +1,11 @@
 import { useReducer, useState } from 'react';
 import photos from '../mocks/photos';
 
+const ACTIONS = {
+  UPDATEFAVPHOTOS: "UpdateFavPhotos", 
+  SETPHOTOSELECTED: "SetPhotoSelected"
+}
+
 // Note: Rendering a single component to build components in isolation
 const useApplicationData = () => {  
 
@@ -15,12 +20,12 @@ const useApplicationData = () => {
   const reducer = function(state, action) {
     let newState = {...state};
     switch (action.type) {
-      case "UpdateFavPhotos":
+      case ACTIONS.UPDATEFAVPHOTOS:
         let newLikedPhotos = {...state.likedPhotos};
         newLikedPhotos[action.value] = newLikedPhotos[action.value] ? false : true;
         newState.likedPhotos = newLikedPhotos;
         return newState;
-      case "SetPhotoSelected":
+      case ACTIONS.SETPHOTOSELECTED:
         newState.photoSelected = action.value;
         return newState;
       default:
@@ -31,15 +36,15 @@ const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const updateToFavPhotoIds = function(id) {
-    dispatch({type: "UpdateFavPhotos", value: id});
+    dispatch({type: ACTIONS.UPDATEFAVPHOTOS, value: id});
   }
 
   const setPhotoSelected = function(photoDetailsId) {
-    dispatch({type: "SetPhotoSelected", value: photoDetailsId})
+    dispatch({type: ACTIONS.SETPHOTOSELECTED, value: photoDetailsId})
   }
   
   const onClosePhotoDetailsModal = function() {
-    dispatch({type: "SetPhotoSelected", value: undefined})
+    dispatch({type: ACTIONS.SETPHOTOSELECTED, value: undefined})
   }
    
   return {
