4778a528946936e3e229437a805eeb048bc0c2d5
Mike Dragert <mikedragert@gmail.com>
2024-02-23 14:13:54 -0500

    Remove React import in useApplicationData.js

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import photos from '../mocks/photos';
 
 // Note: Rendering a single component to build components in isolation
